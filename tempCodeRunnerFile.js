e456212eabc3ea6ce0c0c80004603dfb92177df1
Saurabh Singh <saurabhsingh26rajput@gmail.com>
2025-04-30 16:17:24 +0530

    first

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,5 @@
-const numbers = [10,20,30,40,50,60,70];
-// const[a,b,...rest]= numbers
-// console.log(numbers)
+console.log("hi there ");
+// add(4,5);
+// function add(x, y){
+//     return x + y;
+// }
